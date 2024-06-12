b40b4f04cd9b82ac4b4ddd6ce82240d5d5e3a594
N7Gh0sTT <127984934+N7Gh0sTT@users.noreply.github.com>
2024-06-12 15:59:37 +0200

    added report section

---
+++ errors.js
@@ -0,0 +1,38 @@
+function initializeErrorsDiv() {
+    fetch('http://212.227.26.203:3000/messages')
+        .then(response => response.json())
+        .then(messages => {
+            const errorMessagesDiv = document.getElementById('errorMessages');
+            errorMessagesDiv.innerHTML = ''; 
+            messages.reverse().forEach(message => {
+                const messageDiv = document.createElement('div');
+                messageDiv.classList.add('message');
+
+                const contentSpan = document.createElement('div');
+                contentSpan.classList.add('message-content');
+                contentSpan.textContent = message.content;
+                messageDiv.appendChild(contentSpan);
+
+                if (message.image) {
+                    const imageElement = document.createElement('img');
+                    imageElement.src = message.image;
+                    imageElement.classList.add('message-image');
+                    messageDiv.appendChild(imageElement);
+                }
+
+                if (message.reactions.length > 0) {
+                    const reactionsDiv = document.createElement('div');
+                    reactionsDiv.classList.add('message-reactions');
+                    message.reactions.forEach(reaction => {
+                        const reactionSpan = document.createElement('span');
+                        reactionSpan.textContent = reaction;
+                        reactionsDiv.appendChild(reactionSpan);
+                    });
+                    messageDiv.appendChild(reactionsDiv);
+                }
+
+                errorMessagesDiv.appendChild(messageDiv);
+            });
+        })
+        .catch(error => console.error('Erreur:', error));
+}
---
+++ report.js
@@ -0,0 +1,31 @@
+function initializeReportForm() {
+    const reportForm = document.getElementById('reportForm');
+    if (reportForm) {
+        reportForm.addEventListener('submit', function (event) {
+            event.preventDefault();
+
+            const formData = new FormData();
+            formData.append('description', document.getElementById('description').value);
+            formData.append('screenshot', document.getElementById('screenshot').files[0]);
+            const backendUrl = 'http://212.227.26.203:3000/report';
+            fetch(backendUrl, {
+                method: 'POST',
+                body: formData
+            })
+                .then(response => {
+                    if (response.ok) {
+                        return response.text();
+                    }
+                    throw new Error('Network response was not ok.');
+                })
+                .then(data => {
+                    alert(data);
+                    document.getElementById('reportForm').reset();
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                    alert('Error while sending message.');
+                });
+        });
+    }
+}
